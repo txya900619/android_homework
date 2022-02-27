82d3be9d30ce2fed8aed1586e3c88da409d4fbdd
txya900619 <wayne900619@gmail.com>
2022-02-28 00:26:30 +0800

    init

---
+++ 108820038_HW1/app/src/main/java/com/homework/helloworld/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.homework.helloworld
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        Log.d("MainActivity", "Hello World")
+        Log.i("MainActivity", "MainActivity layout is complete")
+        Log.e("MainActivity","e")
+        Log.v("MainActivity","v")
+        Log.w("MainActivity","w")
+        Log.wtf("MainActivity","wtf")
+    }
+}
